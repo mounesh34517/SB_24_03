449ba95acaff43002ce3a32e04a372ecf4976117
Umesh <nageshbabu0601@gmail.com>
2025-07-03 14:53:56 +0530

    Initial Commit for my project

---
+++ src/main/java/com/paritech/Sb2403Application.java
@@ -0,0 +1,13 @@
+package com.paritech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb2403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb2403Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/paritech/Sb2403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paritech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb2403ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
